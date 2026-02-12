cfaf598e035378441c0bdde0dfff70dd1c3c42d2
marc-rene <95175463+marc-rene@users.noreply.github.com>
2026-02-12 17:06:52 +0000

    Fixed AI signals and tests work

--- Godot/LMM Test/aichat.gd
+++ Godot/LMM Test/aichat.gd
@@ -5,16 +5,18 @@ extends Control
 @onready var aiChat = $NobodyWhoChat
 
 func ask():
-	textEdit.editable = false
-	aiChat.ask(textEdit.text)
-	
+    textEdit.editable = false
+    aiChat.ask(textEdit.text)
+    
 func _input(event:InputEvent) -> void:
-		if(event.is_action_pressed("ui_text_newline")):
-			ask()
+        if(event.is_action_pressed("ui_text_newline")):
+            ask()
 
 func _on_nobody_who_chat_response_updated(new_token: String) -> void:
-		aiText.text += new_token
+        aiText.text += new_token
+        
 
 func _on_nobody_who_chat_response_finished(_response:String) -> void:
-	textEdit.editable = true
-	textEdit.text = ""
+    textEdit.editable = true
+    textEdit.text = ""
+    print("Ai said: " + _response)
